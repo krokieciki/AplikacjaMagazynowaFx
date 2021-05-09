f71288c2e60473163b280146a62ff883a061607d
Namrox <80336908+Namrox@users.noreply.github.com>
2021-05-09 20:15:54 +0200

    123

--- src/sample/HomePageController.java
+++ src/sample/HomePageController.java
@@ -1,5 +1,40 @@
 package sample;
 
-public class HomePageController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HomePageController implements Initializable {
+
+    @FXML
+    private Button exitButton;
+    @FXML
+    private ImageView brandingImageView;
+    @FXML
+    private ImageView bagImageView;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+        File brandingFile = new File("@../../images/HomePageGrafika.png");
+        Image brandingImage = new Image(brandingFile.toURI().toString());
+        brandingImageView.setImage(brandingImage);
+
+        File bagFile = new File("@../../images/work-icon-4454.png");
+        Image lockImage = new Image(bagFile.toURI().toString());
+        bagImageView.setImage(lockImage);
+    }
+
+    public void exitButtonOnAction(ActionEvent event){
+        Stage stage = (Stage) exitButton.getScene().getWindow();
+        stage.close();
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,13 +5,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.setScene(new Scene(root, 520, 400));
         primaryStage.show();
     }
