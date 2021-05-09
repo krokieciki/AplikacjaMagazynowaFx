9e60192b8f06791d651b0ef490cf5d534a532423
Piotrek <80336944+krokieciki@users.noreply.github.com>
2021-05-09 18:45:32 +0200

    poprawka

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 520, 400));
         primaryStage.show();
